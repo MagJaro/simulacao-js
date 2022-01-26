ccf472c92e13f4dc0b14e20658e80214b30b1b9b
Mag <magda.jarozeski.pb@compasso.com.br>
2022-01-26 11:24:41 -0300

    Questao2

---
+++ questao2.js
@@ -0,0 +1,12 @@
+//faça uma função que receba cinco números e retorne a soma deles. use o parametro padrao de 0
+
+const num1 = 5;
+const num2 = 10;
+const num3 = 15;
+const num4 = 20;
+const num5 = 25;
+
+function soma(){
+    return num1 + num2 + num3 + num4 + num5;
+}
+console.log(soma()); //75
