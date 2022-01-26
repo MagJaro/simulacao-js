39870557d7ce517c27c8aff2a1734f9e633bb026
Mag <magda.jarozeski.pb@compasso.com.br>
2022-01-26 11:48:54 -0300

    Questao3

---
+++ questao3.js
@@ -0,0 +1,7 @@
+//Crie um laço de repetição que coloque 100 numeros na tela, 
+//simulando uma contagem regressiva de uma bomba (em ordem decrescente)
+
+
+for (let contador = 100;  contador > 0 ; contador--){
+    console.log(contador);
+}
